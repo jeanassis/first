b4c97054e59bc02fe855753bb99d0633c73facbc
jeanassis <jeansiqueira06@gmail.com>
2026-01-04 17:41:41 -0300

    first commit

---
+++ src/test/java/desenv/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package desenv.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
